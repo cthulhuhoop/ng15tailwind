50a953179b712a193f1c6abf14782878b1f8ad7c
akj <adrian_k_jones@hotmail.com>
2023-01-30 20:11:28 +0000

    reformat

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { IUser } from './models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor() {}
 
-  constructor() { }
-
-  getUsers() : IUser[] {
-    return [{id:1, name: 'fred'},{id:2, name: 'barney'},{id:3, name: 'wilma'},{id:4, name: 'pebbles'}]
+  getUsers(): IUser[] {
+    return [
+      { id: 1, name: 'fred' },
+      { id: 2, name: 'barney' },
+      { id: 3, name: 'wilma' },
+      { id: 4, name: 'pebbles' },
+    ];
   }
 }
